f71648e1bd5b2a1aef8b70c758dc0ef0c0c8c382
Bruce Goldfeder <bgoldfe2@masonlive.gmu.edu>
2019-12-23 22:19:39 +0000

    looks good working on final graphs

--- BruceG_v4.r
+++ BruceG_v4.r
@@ -4,13 +4,16 @@
 
 # Load the Iso package
 #install.packages("Iso", repos='http://cran.us.r-project.org')
+#install.packages('dplyr')
+
 library(Iso)
+#library(dplyr)
 
 # Generate pseudo-random numbers in normal distribution
 #set.seed(123)
 set.seed(144)
 
-size<-100
+size<-1500
 x<-rnorm(size)
 y<-sort(x,decreasing=FALSE)
 hist(y)
@@ -20,40 +23,35 @@ plot(y)
 # Output the g vector
 n<-size
 g=c()
-prev<-x[1]  # the j-1 variable
+prev<-y[1]  # the j-1 variable
 curr<-0.1   # the j variable this should be overwritten in loop
 # The loop requires two elements in x and results in a vector of length size-1 or 1499 elements
 for (j in 2:size) {
-    curr<-x[j]
-    g[j-1]=1/(n*(curr-prev))
-    prev<-x[j]
+    curr<-y[j]
+    #g[j-1]=1/(n*(curr-prev))
+    g[j-1]<-1/n*((y[j]-y[j-1])/abs(y[j]))
+    prev<-y[j]
 }
 
-plot(g)
-gsort<-sort(g,decreasing=FALSE)
-plot(gsort)
+plot(x[1:size-1],g)
+#gsort<-sort(g,decreasing=FALSE)
+#plot(x[1:size-1],gsort)
 
 # Run the pool adjacent violators algorithm
-# STEPFUN do I use this or not?
-h<-pava(g,stepfun = TRUE)
-#plot(y[1:size-1],h)  # I seem to not use this to solve for MLE???
-plot(h,verticals=FALSE,pch=20,col.points="blue")
-h2<-pava(g,decreasing = TRUE,stepfun = TRUE)
-plot(h2,add=TRUE,verticals=FALSE,pch=20,col.points="red")
+
+h<-pava(g,decreasing = FALSE)
+plot(y[1:size-1],h)
+
+h2<-pava(g,decreasing = TRUE)  # stepfun gives the edge numbers
+plot(y[1:size-1],h2)
 
 #h2<-pava(g,decreasing=TRUE)
 #plot(y[1:size-1],h2,pch=21,bg="blue")
-#lines(y[1:size-1],h2,pch=21,bg="blue")
+#lines(y[1:size-1],h2,pch=21,bg="blue"
 
 # Guessing on this one
 #hleft<-ufit(h,x=y[size-1],lmode=leftmd,lc=TRUE)
 #plot(hleft)
-
-# This section depicts the manual discovery of the mode using the left-most point of the right most bar of points
-#h[1500]
-#h[1000]
-#h[800]
-#h[900]
 
 # Find the min and max for x the original normal distribution
 leftmd<-min(x)
@@ -61,34 +59,40 @@ rightmd<-max(x)
 
 # Solve using ufit with g the raw density and y the ordered normal distribution
 # to find the "interval" where the overlap we need to determine MLE
-right<-ufit(g,x=y[1:1499],lmode=rightmd,rc=TRUE)
+
+right<-ufit(g,x=y[1:size-1],lmode=rightmd,rc=TRUE)
 pavright<-pava(right$y)
-plot(y[1:1499],pavright)
+plot(y[1:size-1],pavright)
 lines(right)
-left<-ufit(g,x=y[1:1499],lmode=leftmd,lc=TRUE)
+left<-ufit(g,x=y[1:size-1],lmode=leftmd,lc=TRUE)
 pavleft<-pava(left$y,decreasing=TRUE)
-lines(y[1:1499],pavleft,bg="blue")
+lines(y[1:size-1],pavleft,bg="blue")
 lines(left)
-
-#right
-#left
-
-# Via visual inspection find the overlapping elements from 
-# The left and the right and use M1 as the mode
-# This is automated in code below
-# Convert those to y values as the two modes to find MLE
 
 # Find the last element of the left entering value (top left of graph)
 # Find the first element of the right leaving value (top right of graph)
-lval<-left$y[1]
-lidx<-length(which(left$y == lval))
-rval<-right$y[1499]
-ridx<-1499-length(which(right$y == rval))
+# lval<-left$y[1] # This implementation was a bit naive and did not take into account minor roundoff errors
+
+# Finds the most common value in the large set of values coming in from the left (eliminates roundoff errors)
+leftin<-sort(table(pavleft[1:800]),decreasing=TRUE)[1]
+lval<-names(leftin)[1]
+# Finds the last index of the entry line coming in from the left
+lidx<-max(which(pavleft %in% c(lval)))
+
+# Finds the most common value in the large set of values coming in from the right (eliminates roundoff errors)
+rightin<-sort(table(pavright[size-1:700]),decreasing=TRUE)[1]
+rval<-names(rightin)[1]
+# Finds the first index of the entry line coming in from the right
+ridx<-match(c(rval),pavright)
 
 lelist<-c()
+yhatmx<-matrix(NA, nrow=lidx-ridx+1, ncol=size-1)
+cnt<-0
 for (i in lidx:ridx) {
-  fhatx1m1<-ufit(g,x=y[1:1499],lmode=y[i])
+  cnt<-cnt+1
+  fhatx1m1<-ufit(g,x=y[1:size-1],lmode=y[i])
   yhat<-fhatx1m1$y
+  yhatmx[cnt,]<-yhat
   yhat<-yhat*sign(yhat)  # make all values positive for log-likelihood
   logyhat<-log(yhat)
   like<-sum(logyhat)
@@ -99,4 +103,8 @@ MLE<-min(lelist,na.rm=TRUE)  # This is the most negative log value which is the 
 MLEidx<-which(lelist == MLE)
 MLEval<-y[MLEidx]
 
+# For log-likelihood the lowest negative value will generate the least negative number
+plot(lelist[lidx:ridx])
+
 MLEval
+MLEidx
